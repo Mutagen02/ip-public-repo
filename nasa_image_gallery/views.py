151b8b6026714a536a67be01d9eba41cbd26592f
Angelschnd <angelschvind2@gmail.com>
2024-06-24 21:15:59 -0300

    otro cambio en "views
    se agregó la funcion getAllImages

--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -12,7 +12,7 @@ def index_page(request):
 
 # auxiliar: retorna 2 listados -> uno de las imágenes de la API y otro de los favoritos del usuario.
 def getAllImagesAndFavouriteList(request):
-    images = 
+    images = getAllImages(input=none)
     favourite_list = getAllFavouritesByUser(request) 
 
     return images, favourite_list
