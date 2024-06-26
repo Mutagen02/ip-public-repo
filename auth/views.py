9b75af4a1e00cd0642af93d6c484addeadc81a28
Angelschnd <angelschvind2@gmail.com>
2024-06-25 22:35:09 -0300

    Update views.py

--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    images, favourite_list = []
-    return render(request, "home.html", {"images" : images, "favourite_list", favourite_list})
