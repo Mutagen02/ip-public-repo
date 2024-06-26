20c2bbd2ee0d70c4172e600b9fc0dcf4ba4c9534
Angelschnd <angelschvind2@gmail.com>
2024-06-25 23:02:56 -0300

    ..

--- nasa_image_gallery/layers/transport/transport.py
+++ nasa_image_gallery/layers/transport/transport.py
@@ -6,11 +6,11 @@ from ...config import config
 # comunicación con la REST API de la NASA.
 def getAllImages(input=None):
     if input is None:
-        json_response = requests.get(config.NASA_REST_API_DEFAULT_SEARCH).json()
+        json_response = str(requests.get(config.NASA_REST_API_DEFAULT_SEARCH).json())
     else:
-        json_response = requests.get(config.NASA_REST_API + input).json()
+        json_response = str(requests.get(config.NASA_REST_API + input).json())
 
-    str(json_collection) = []
+    json_collection = []
     for object in json_response['collection']['items']:
         try:
             if 'links' in object:  # verificar si la clave 'links' está presente en el objeto (sin 'links' NO nos sirve, ya que no mostrará las imágenes).
