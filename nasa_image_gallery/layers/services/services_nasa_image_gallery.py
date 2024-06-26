798352234084e175a6d9f5f471fe5f6bdb88b37f
Angelschnd <angelschvind2@gmail.com>
2024-06-25 23:34:44 -0300

    Update services_nasa_image_gallery.py

--- nasa_image_gallery/layers/services/services_nasa_image_gallery.py
+++ nasa_image_gallery/layers/services/services_nasa_image_gallery.py
@@ -5,10 +5,10 @@ from ..dao import repositories
 from ..generic import mapper
 from django.contrib.auth import get_user
 
-def getAllImages(request):
+def getAllImages(input=None):
     # obtiene un listado de imágenes desde transport.py y lo guarda en un json_collection.
     # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
-    json_collection = transport.getAllImages(request)
+    json_collection = transport.getAllImages()
     images = []
 
     for element in json_collection:
